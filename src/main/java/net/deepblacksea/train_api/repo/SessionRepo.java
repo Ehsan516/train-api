d82679ae561da38e834329eb7bdaa060e85968ea
Ehsan Ahmed <ehsan.ahmed626@gmail.com>
2025-09-13 12:49:22 +0100

    checking if repo listed in git

--- src/main/java/net/deepblacksea/train_api/repo/SessionRepo.java
+++ src/main/java/net/deepblacksea/train_api/repo/SessionRepo.java
@@ -1,2 +1,11 @@
-package net.deepblacksea.train_api.repo;public class SessionRepo {
+//repo for session table
+package net.deepblacksea.train_api.repo;
+
+import net.deepblacksea.train_api.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+public interface SessionRepo extends JpaRepository<Session, UUID> {
+    //recent session by sorting by descending
+    List<Session> findByUserIdOrderByStartedAtDesc(UUID userId);
 }
--- src/main/java/net/deepblacksea/train_api/repo/SetRepo.java
+++ src/main/java/net/deepblacksea/train_api/repo/SetRepo.java
@@ -1,2 +1,10 @@
-package net.deepblacksea.train_api.repo;public class SetRepo {
+//repo for sets table
+package net.deepblacksea.train_api.repo;
+
+import net.deepblacksea.train_api.model.SetEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+public interface SetRepo extends JpaRepository<SetEntry, UUID> {
+    List<SetEntry> findBySessionIdOrderByIdAsc(UUID sessionId);
 }
